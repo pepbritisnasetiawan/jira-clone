07b1412ec64dc5c550f5d8039775a7720fb0fa42
pepbritisnasetiawan <pepbritisnasetiawan@gmail.com>
2024-10-22 17:30:39 +0700

    modify: high on input and select

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
 
 export default function Home() {
   return (
     <div className="">
+      <Input />
       <Button size="lg">Primary</Button>
       <Button variant="secondary">Secondary</Button>
       <Button variant="destructive">Destructive</Button>
