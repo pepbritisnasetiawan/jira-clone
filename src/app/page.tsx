6a20281dcbeacf1ee4dcd6bbbc340f55c6b3b6c9
pepbritisnasetiawan <pepbritisnasetiawan@gmail.com>
2024-10-22 17:24:49 +0700

    change: button variant and button size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,13 @@ import { Button } from '@/components/ui/button';
 export default function Home() {
   return (
     <div className="">
-      <Button>Primary</Button>
+      <Button size="lg">Primary</Button>
       <Button variant="secondary">Secondary</Button>
       <Button variant="destructive">Destructive</Button>
       <Button variant="ghost">Ghost</Button>
-      <Button variant="link">Link</Button>
+      <Button variant="muted">Mted</Button>
       <Button variant="outline">Outlie</Button>
+      <Button variant="teritary">Teritary</Button>
     </div>
   );
 }
