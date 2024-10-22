c4b187569fdfcab6be1804e833e80d095ffad3cc
pepbritisnasetiawan <pepbritisnasetiawan@gmail.com>
2024-10-22 17:08:06 +0700

    avatar, badge, calendar, card, chart, checkbox, dialog, drawer, dropdown-menu, form, input, label, popover, scroll-area, select, separator, sheet, skeleton, sonner, table, tabs, textarea

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,13 @@ import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <div>
-      <Button size="sm">Click Me</Button>
+    <div className="">
+      <Button>Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="destructive">Destructive</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="link">Link</Button>
+      <Button variant="outline">Outlie</Button>
     </div>
   );
 }
---
+++ src/features/test.tsx
@@ -0,0 +1,3 @@
+export const TestComponent = () => {
+  return <div>Hello Component</div>;
+};
