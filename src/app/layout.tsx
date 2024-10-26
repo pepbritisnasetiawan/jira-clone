255a99db0bd5ac48c01731bbae811834d835510b
pepbritisnasetiawan <pepbritisnasetiawan@gmail.com>
2024-10-26 12:27:16 +0700

    add: creating auth api

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
 import { cn } from '@/lib/utils';
+import { QueryProvider } from '@/components/query-provider';
 
 import './globals.css';
 
@@ -19,10 +20,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={cn(inter.className, "antialiased min-h-screen")}
-      >
-        {children}
+      <body className={cn(inter.className, 'antialiased min-h-screen')}>
+        <QueryProvider>{children}</QueryProvider>
       </body>
     </html>
   );
