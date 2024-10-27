1fee117f627fc8f50ebe8cfb6c7ca315ff07fc60
pepbritisnasetiawan <pepbritisnasetiawan@gmail.com>
2024-10-28 00:50:58 +0700

    setting up upwrite & database

--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -111,7 +111,7 @@ export const SignUpCard = () => {
               )}
             />
             <Button disabled={false} size="lg" className="w-full">
-              Login
+              Register
             </Button>
           </form>
         </Form>
---
+++ src/features/auth/constants.ts
@@ -0,0 +1,1 @@
+export const AUTH_COOKIE = 'cft-jita-clone-session';
