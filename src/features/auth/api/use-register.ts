040a817dd965ea9ca69f131e577951ef82677dc5
pepbritisnasetiawan <pepbritisnasetiawan@gmail.com>
2024-10-29 11:13:30 +0700

    Add: workspace form

--- src/features/auth/api/use-register.ts
+++ src/features/auth/api/use-register.ts
@@ -1,8 +1,6 @@
+import { toast } from 'sonner';
 import { useRouter } from 'next/navigation';
-import {
-  useMutation,
-  useQueryClient,
-} from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { InferRequestType, InferResponseType } from 'hono';
 
 import { client } from '@/lib/rpc';
@@ -19,11 +17,20 @@ export const useRegister = () => {
   const mutation = useMutation<ResponseType, Error, RequestType>({
     mutationFn: async ({ json }) => {
       const response = await client.api.auth.register['$post']({ json });
+
+      if (!response.ok) {
+        throw new Error('Failed to register!');
+      }
+
       return await response.json();
     },
     onSuccess: () => {
+      toast.success('Registered');
       router.refresh();
-      queryClient.invalidateQueries({ queryKey: ['current' ] });
+      queryClient.invalidateQueries({ queryKey: ['current'] });
+    },
+    onError: () => {
+      toast.error('Failed to register');
     },
   });
 
---
+++ src/features/workspaces/server/route.ts
@@ -0,0 +1,31 @@
+import { zValidator } from '@hono/zod-validator';
+import { Hono } from 'hono';
+import { createWorkspacesSchema } from '../schemas';
+import { sessionMiddleware } from '@/lib/session-middleware';
+import { DATABASE_ID, WORKSPACES_ID } from '@/config';
+import { ID } from 'node-appwrite';
+
+const app = new Hono().post(
+  '/',
+  zValidator('json', createWorkspacesSchema),
+  sessionMiddleware,
+  async (c) => {
+    const databases = c.get('databases');
+    const user = c.get('user');
+
+    const { name } = c.req.valid('json');
+
+    const workspaces = await databases.createDocument(
+      DATABASE_ID,
+      WORKSPACES_ID,
+      ID.unique(),
+      {
+        name,
+        userId: user.$id
+      }
+    );
+    return c.json({ data: workspaces });
+  }
+);
+
+export default app;
