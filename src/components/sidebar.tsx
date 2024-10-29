5f474da00660ee228495cdca7bded67fabff138b
pepbritisnasetiawan <pepbritisnasetiawan@gmail.com>
2024-10-29 10:08:12 +0700

    Add: Dashboard layout

---
+++ src/components/sidebar.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { Navigation } from './navigation';
+import { DottedSeparator } from './dotted-separator';
+
+export const Sidebar = () => {
+  return (
+    <aside className="h-full bg-neutral-100 p-4 w-full">
+      <Link href="/">
+        <Image src="/logo.svg" alt="logo" width={90} height={50} />
+      </Link>
+      <div className="px-7">
+        <DottedSeparator className="my-4" />
+      </div>
+
+      <Navigation />
+    </aside>
+  );
+};
